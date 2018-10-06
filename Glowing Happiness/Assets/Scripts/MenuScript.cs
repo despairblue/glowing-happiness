15c7487dd4c6fc1f7ae6208d1df4359a46eec556
DESKTOP-5US3FU0\Niaz <tigrexspalterlp@gmail.com>
2018-10-06 16:05:59 +0200

    wipwipwip

---
+++ Glowing Happiness/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour {
+
+    void Start () {
+        PlayerPrefs.SetInt("confession", 0);
+	}
+	
+}
--- Glowing Happiness/Assets/Scripts/UI/ConfessionUI.cs
+++ Glowing Happiness/Assets/Scripts/UI/ConfessionUI.cs
@@ -1,16 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ConfessionUI : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+    private Slider slider;
+
+    private void Start()
+    {
+        slider = GetComponent<Slider>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        slider.value = PlayerPrefs.GetInt("confession");
 	}
 }
