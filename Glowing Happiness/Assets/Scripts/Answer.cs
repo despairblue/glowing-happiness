49149fbf8648ec8bf3e92e66c535cdfac1e7d044
DESKTOP-5US3FU0\Niaz <tigrexspalterlp@gmail.com>
2018-10-06 12:59:20 +0200

    wip

---
+++ Glowing Happiness/Assets/Scripts/Answer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class Answer : MonoBehaviour {
+
+    private Question Question;
+    private TextMeshProUGUI AnswerObejct;
+    public string text;
+    public string Reaction;
+    public string Scene;
+    public int ConfessionDelta;
+
+
+    public void Start()
+    {
+        Question = GetComponentInParent<Question>();
+        AnswerObejct = GetComponentInChildren<TextMeshProUGUI>();
+        AnswerObejct.text = text;
+    }
+
+    public void OnClick()
+    {
+        UpdateConfessionMeter();
+        React();
+        //Wait for Player Input
+        SceneManager.LoadScene(Scene);
+    }
+
+    private void UpdateConfessionMeter() {
+        int currentConfession = PlayerPrefs.GetInt("confession");
+        PlayerPrefs.SetInt("confession", currentConfession + ConfessionDelta);
+    }
+
+    private void React() {
+        Question.setText(Reaction);
+    }
+
+    
+}
---
+++ Glowing Happiness/Assets/Scripts/Question.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class Question : MonoBehaviour {
+
+    private TextMeshProUGUI Text;
+    public string question;
+
+    public void Start()
+    {
+        Text = GetComponentInChildren<TextMeshProUGUI>();
+        setText(question);
+    }
+
+    public void setText(string text) {
+        Text.text = text;
+    }
+
+}
