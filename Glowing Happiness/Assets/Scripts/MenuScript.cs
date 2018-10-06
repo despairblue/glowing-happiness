f1afe9359c5607b6ee9fe1c27cfa1104e5c827b4
DESKTOP-5US3FU0\Niaz <tigrexspalterlp@gmail.com>
2018-10-06 16:08:42 +0200

    wipwipwip

--- Glowing Happiness/Assets/Scripts/MenuScript.cs
+++ Glowing Happiness/Assets/Scripts/MenuScript.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour {
 
     void Start () {
         PlayerPrefs.SetInt("confession", 0);
+        SceneManager.LoadScene("Main");
 	}
 	
 }
