445e3e058f0ff8fadeb9b85de9994baa279c8f3b
DESKTOP-5US3FU0\Niaz <tigrexspalterlp@gmail.com>
2018-10-06 12:22:41 +0200

    wip

---
+++ Glowing Happiness/Assets/Scripts/DialogOption.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class DialogOption : MonoBehaviour {
+
+    public TextMeshProUGUI Text;
+    public string scene;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void onClick() {
+        SceneManager.LoadScene(scene);
+    }
+}
